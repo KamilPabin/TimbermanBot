b4727dcf2d6f6cefa4702dbdb8bcd200d2be88a4
Kamil Pabin <pabin.kamil@gmail.com>
2021-10-22 22:26:40 +0200

    Improved screen capture

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys
 import time
 import keyboard
+import pyautogui as pyautogui
+import numpy as np
+import cv2
+from mss import mss
+
 from PIL import ImageGrab
 
 # 0, 0      720, 0     1440, 0
@@ -19,50 +24,37 @@ branch_width = 150
 left_branch_bounding_box = (middle_x - block_width / 2 - 110, height - offset_y - block_height - 29 - 60, middle_x - block_width / 2, height - offset_y - block_height - 29)
 right_branch_bounding_box = (middle_x + block_width / 2, height - offset_y - block_height - 29 - 60, middle_x + block_width / 2 + 110, height - offset_y - block_height - 29)
 
-save_printscreen = True
-
-
-def stop_program():
-    sys.exit(1)
-
-def save_screenshot(msg: str, image: Image):
-    if save_printscreen:
-        image.save(f"{msg}.png")
-
-
-keyboard.on_press_key("x", lambda _: stop_program())
+left_monitor = {"top": height - offset_y - block_height - 29 - 60, "left": middle_x - block_width / 2 - 110, "width": 110, "height": 60}
+right_monitor = {"top": height - offset_y - block_height - 29 - 60, "left": middle_x + block_width / 2, "width": 110, "height": 60}
 
 
 keyboard.wait('s')
 print("started")
-left_background = ImageGrab.grab(bbox=left_branch_bounding_box)
-right_background = ImageGrab.grab(bbox=right_branch_bounding_box)
+with mss() as sct:
+    left_background = sct.grab(left_monitor)
+    right_background = sct.grab(right_monitor)
 
-save_screenshot("left", left_background)
-save_screenshot("right", right_background)
-side = 0
-i = 0
-while True:
-    ImageGrab.grab().save(f"fullscreen{i}.png")
-    if side == 0:
-        im = ImageGrab.grab(bbox=left_branch_bounding_box)
-        save_screenshot(i, im)
-        is_occupied = list(left_background.getdata()) != list(im.getdata())
-        print(f"{i}: side: {side} is occupied: {is_occupied}")
-        if is_occupied:
-            side = 1
-            keyboard.write("l")
+    side = 0
+    i = 0
+    while True:
+        if side == 0:
+            im = sct.grab(left_monitor)
+            is_occupied = list(im.raw) != list(left_background.raw)
+            print(f"{i}: side: {side} is occupied: {is_occupied}")
+            if is_occupied:
+                side = 1
+                keyboard.write("l")
+            else:
+                keyboard.write("a")
         else:
-            keyboard.write("a")
-    else:
-        im = ImageGrab.grab(bbox=right_branch_bounding_box)
-        save_screenshot(i, im)
-        is_occupied = list(right_background.getdata()) != list(im.getdata())
-        print(f"{i}: side: {side} is occupied: {is_occupied}")
-        if is_occupied:
-            side = 0
-            keyboard.write("a")
-        else:
-            keyboard.write("l")
-    i = i + 1
-    time.sleep(0.1)
+            im = sct.grab(right_monitor)
+            is_occupied = list(im.raw) != list(right_background.raw)
+            print(f"{i}: side: {side} is occupied: {is_occupied}")
+            if is_occupied:
+                side = 0
+                keyboard.write("a")
+            else:
+                keyboard.write("l")
+        i = i + 1
+        time.sleep(0.3)
+
